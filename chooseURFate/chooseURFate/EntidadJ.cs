5694a241eb03b4b5c5e8f2ad1ec9a21527c7c9e0
Elena Guzmán Blanco <elena.guzbla@gmail.com>
2016-03-26 16:40:17 +0100

    Anadida Clase abstracta

---
+++ chooseURFate/chooseURFate/EntidadJ.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chooseURFate
+{
+    abstract class EntidadJugador
+    {
+        int limitePob;
+        int limiteRecursos;
+        int Edificios;
+
+        int nViviendas;
+        int nGranjas;
+        int nMadereros;
+        int nMinaOro;
+        int nMinaPiedra;
+
+
+    }
+}
